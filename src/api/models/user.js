182351885174969ffe0e98a4a04eb7eb9cb2df4b
Guille <guillempaniagua@gmail.com>
2026-04-28 13:39:05 +0000

    feat: modelo de usuario con roles

---
+++ src/api/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user'
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const User = mongoose.model('users', userSchema)
+
+module.exports = User
