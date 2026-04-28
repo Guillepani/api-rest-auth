891fdaf15e3a55b5139920a6a15049b1b417ec24
Guille <guillempaniagua@gmail.com>
2026-04-28 14:01:24 +0000

    feat: login con JWT implementado

--- src/api/controllers/auth.js
+++ src/api/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   try {
@@ -16,4 +17,30 @@ const register = async (req, res) => {
   }
 }
 
-module.exports = { register }
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body
+
+    const user = await User.findOne({ username })
+
+    if (!user) {
+      return res.status(400).json('Usuario no encontrado')
+    }
+
+    const isMatch = bcrypt.compareSync(password, user.password)
+
+    if (!isMatch) {
+      return res.status(400).json('Password incorrecta')
+    }
+
+    const token = jwt.sign({ id: user._id, role: user.role }, 'secreto', {
+      expiresIn: '1h'
+    })
+
+    return res.status(200).json({ token })
+  } catch (error) {
+    return res.status(400).json('Error en login')
+  }
+}
+
+module.exports = { register, login }
