18d4ed13a912e32276c287f813196f235dd15eb4
Guille <guillempaniagua@gmail.com>
2026-04-28 13:37:57 +0000

    feat: conexión a MongoDB configurada

---
+++ src/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const connectDB = require('./config/db')
+connectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('API funcionando correctamente')
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
