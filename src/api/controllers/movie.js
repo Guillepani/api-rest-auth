67833f9fb53cf8faa0152750d563c18545061d33
Guille <guillempaniagua@gmail.com>
2026-04-28 15:04:39 +0000

    feat: delete de movie funcional con eliminación en Mongo y Cloudinary

--- src/api/controllers/movie.js
+++ src/api/controllers/movie.js
@@ -1,4 +1,5 @@
 const Movie = require('../models/movie')
+const { cloudinary } = require('../../config/cloudinary')
 
 const createMovie = async (req, res) => {
   try {
@@ -12,8 +13,34 @@ const createMovie = async (req, res) => {
 
     return res.status(201).json(movieSaved)
   } catch (error) {
+    console.log(error)
     return res.status(400).json('Error al crear movie')
   }
 }
 
-module.exports = { createMovie }
+const deleteMovie = async (req, res) => {
+  console.log('DELETE HIT')
+  try {
+    const { id } = req.params
+
+    const movieDeleted = await Movie.findByIdAndDelete(id)
+
+    if (!movieDeleted) {
+      return res.status(404).json('Movie no encontrada')
+    }
+
+    if (movieDeleted.img) {
+      const imgUrl = movieDeleted.img
+      const imgName = imgUrl.split('/').pop().split('.')[0]
+
+      await cloudinary.uploader.destroy(`api-rest-files/${imgName}`)
+    }
+
+    return res.status(200).json(movieDeleted)
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json('Error al borrar movie')
+  }
+}
+
+module.exports = { createMovie, deleteMovie }
--- src/api/routes/movie.js
+++ src/api/routes/movie.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { createMovie } = require('../controllers/movie')
+const { createMovie, deleteMovie } = require('../controllers/movie')
 const { upload } = require('../../middlewares/file')
 
 const router = express.Router()
 
 router.post('/', upload.single('img'), createMovie)
+router.delete('/:id', deleteMovie)
 
 module.exports = router
